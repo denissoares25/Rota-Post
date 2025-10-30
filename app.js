f9d80aa343e26fbda939ae8ae4ec9206e8ecb3da
denissoares25 <60358713+denissoares25@users.noreply.github.com>
2025-10-29 21:34:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 const express = require("express");
 const app = express();
 const PORT = 8081;
+const pathFile= "./livros.json";
+const fs = require("fs");
 
 app.use(express.json());
+
+if (!fs.existsSync(pathFile)) {
+fs.writeFileSync(pathFile, `[]`);
+}
+
+app.get("/livros", (req, res)=>{
+    try{
+
+        const data = fs.readFileSync(pathFile, "utf-8");
+
+        let livros = JSON.parse(data);
+
+
+        res.status(200).json(livros);
+    }catch(error){
+        console.error("Erro ao ler o arquivo JSON:", error);
+        res.status(500).json({error: "Erro ao Erro interno no servidor ao processar os produtos!"});
+     }
+});
+
 
 app.post("/livros", (req, res) => {
     try {
         const { titulo, autor, anoPublicação, qtdExemplares } = req.body;
 
-        const produto = {
-            titulo: titulo,
+        const livro = {
+            titulo: titulo, 
             autor: autor,
             anoPublicação: anoPublicação,
             qtdExemplares: qtdExemplares
 
         };
 
+        const data = fs.readFileSync(pathFile,"utf-8");
+        const livros = JSON.parse(data);
+
+        livros.push(livro);
+        
+        fs.writeFileSync(pathFile, JSON.stringify(livros, null, 4));
+        
        
         res.status(201).json({
-            message: "Produto cadastrado com sucesso!",
-            produto: produto
+            message: "Livro cadastrado com sucesso!",
+            livro: livro
         });
     } catch (error) {
-        console.error("erro ao cadastrar produto:", error);
-        res.status(500).json({ error: "erro interno no servidor ao cadastrar produto!" });
+        console.error("erro ao cadastrar livro:", error);
+        res.status(500).json({ error: "erro interno no servidor ao cadastrar livro!" });
     }
 });
 
